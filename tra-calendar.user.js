489da178a1f967a5f29bb9b88b0bed2fe8fce1a7
Still Hsu <5843208+Still34@users.noreply.github.com>
2020-05-29 14:02:44 +0800

    Add updateUrl for auto update
    Signed-off-by: Still Hsu <5843208+Still34@users.noreply.github.com>

--- tra-calendar.user.js
+++ tra-calendar.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name        TRA Calendar Shortcut
-// @version     1.0.0
+// @version     1.0.1
 // @description Adds Google Calendar shortcut to the Taiwan Railway Administration timetable
 // @license     MIT
 // @author      Still Hsu
 // @include     https://www.railway.gov.tw/tra-tip-web/*/*/*/querybytime
 // @inject-into content
+// @downloadUrl https://raw.githubusercontent.com/Still34/tra-shortcut/master/tra-calendar.user.js
+// @updateUrl https://raw.githubusercontent.com/Still34/tra-shortcut/master/tra-calendar.user.js
 // @grant GM_openInTab
 // ==/UserScript==
 
